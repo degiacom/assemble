b554c9c851eb2c5e4f794c4126cd5de072da7b15
degiacom <matteo.degiacomi@gmail.com>
2015-02-14 14:03:59 +0000

    fixed license
    corrected email

--- System.py
+++ System.py
@@ -9,7 +9,7 @@
 # You should have received a copy of the GNU General Public License along with Assemble ;
 # if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
 #
-# Authors : Matteo Degiacomi, matteothomas.degiacomi@gmail.com, Valentina Erastova, valentina.erastova@gmail.com
+# Authors : Matteo Degiacomi, matteo.degiacomi@gmail.com, Valentina Erastova, valentina.erastova@gmail.com
 
 import numpy as np
 import logging
--- Topology.py
+++ Topology.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2014 Matteo Degiacomi and Valentina Erastova
+#
+# Assemble is free software ;
+# you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation ;
+# either version 2 of the License, or (at your option) any later version.
+# Assemble is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY ;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Assemble ;
+# if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+#
+# Authors : Matteo Degiacomi, matteo.degiacomi@gmail.com, Valentina Erastova, valentina.erastova@gmail.com
+
+
 import numpy as np
 
 class Topology(object):
